f139bea2a72f8c34cf454c815f253ec4e220a263
Seth Jeroutek <seth.jeroutek@outlook.com>
2023-12-08 09:20:55 -0500

    Cleaned up repo

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-import csv
 from scipy.io import wavfile
+from scipy.signal import hilbert
 
 # Plots the carrier, information, and modulated signals.
 def graph_signals(signals: list, t: np.array, titles=["Carrier Signal", "Information/AM Signals"]):
@@ -15,7 +15,7 @@ def graph_signals(signals: list, t: np.array, titles=["Carrier Signal", "Informa
     plt.subplot(2, 1, 2)
     plt.title(titles[1])
     plt.plot(t, signals[1])
-    plt.plot(t, signals[2])
+    #plt.plot(t, signals[2])
     plt.legend(['AM Signal', 'Original Signal'])
 
     plt.xlabel("Time (s)")
@@ -24,18 +24,17 @@ def graph_signals(signals: list, t: np.array, titles=["Carrier Signal", "Informa
 # Get command-line arguments.
 def get_arguments():
     if len(sys.argv) < 4:
-        print("Usage: python main.py <audio_file> <output_file> <k> <carrier frequency> <graph [0,1]>")
-        print("                      Filepath of   'graph' must                                 0 to print output to .csv file.")
-        print("                      audio file.   be 1.                                          1 to graph output.")
+        print("Usage: python main.py <audio_file> <output_file> <carrier frequency> <graph [0,1]>")
+        print("                      Filepath of   'graph' must                             0 to print output to .csv file.")
+        print("                      audio file.   be 1.                                      1 to graph output.")
         sys.exit(1)
     else:
         argument = sys.argv[1]
         output = sys.argv[2]
-        k = int(sys.argv[3])
-        c_freq = float(sys.argv[4])
-        graph = int(sys.argv[5])
+        c_freq = float(sys.argv[3])
+        graph = int(sys.argv[4])
 
-    return argument, output, k, c_freq, graph
+    return argument, output, c_freq, graph
 
 # Get the modulating signal from input.
 def get_modulating_signal(audio_file):
@@ -46,24 +45,35 @@ def get_modulating_signal(audio_file):
     return sampling_rate, data
 
 # Read in from command line.
-audio_file, output, k, c_freq, graph = get_arguments()
+audio_file, output, c_freq, graph = get_arguments()
 
 sampling_rate, data = get_modulating_signal(audio_file)
 duration = data.shape[0] / sampling_rate
 t = np.linspace(0, duration, int(sampling_rate * duration), endpoint=False)
 
+#t = np.arange(0, len(data) / sampling_rate, 1 / sampling_rate)
+
 # Create carrier and information signals.
+
+#information = (data / np.max(np.abs(data)))
+#analytic = hilbert(information)
+#carrier = np.abs(analytic) * np.cos(2 * np.pi * c_freq * t)
 carrier = np.cos(2 * np.pi * c_freq * t)
-information = data / max(data)
+
+#k = 0.9 * (k / np.max(np.abs(k)))
+
+#information = np.cos(2 * np.pi * 5 * t)
 
 # Perform AM modulation.
-am_modulated_signal = (1 + (k * information)) * carrier
+am_modulated_signal = (1 + (0.5 * data)) * carrier
+
+#am_modulated_signal = am_modulated_signal / max(am_modulated_signal)
 
 if (graph == 1): 
-    graph_signals([carrier, am_modulated_signal, information], t)
+    graph_signals([carrier, am_modulated_signal, data], t)
 else:
-    # Organize waveform into time and voltage columns.
-    data = np.column_stack((t, am_modulated_signal))
+    # Organize waveform into column of data.
+    data = np.column_stack(am_modulated_signal)
 
     # Save the data to a CSV file.
     np.savetxt(output, data, delimiter=',', comments='')
--- tts.py
+++ tts.py
@@ -2,7 +2,7 @@ import pyttsx3
 import sys
 
 def get_arguments():
-    if len(sys.argv) < 7:
+    if len(sys.argv) < 6:
         print("Usage: python tts.py <text> <filename>  <rate [0-200]> <volume [0.0-1.0]> <voice [0,1]>")
         print("                            Filepath of  Words per      Quiet to loud.            0 for male")
         print("                            output file. minute.                                    1 for female")
@@ -18,14 +18,13 @@ def get_arguments():
 
 text, filename, rate, volume, voice, = get_arguments()
 
-engine = pyttsx3.init() # object creation
+engine = pyttsx3.init()
 
-engine.setProperty('rate', int(rate))        # Rate of talking.
-engine.setProperty('volume', float(volume))    # Volume of talking.
+engine.setProperty('rate', int(rate))               # Rate of talking.
+engine.setProperty('volume', float(volume))         # Volume of talking.
 
-# Apply whispering effect (if available in the voice)
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[int(voice)].id)  # Use the second voice, assuming it supports whispering
+engine.setProperty('voice', voices[int(voice)].id)  # Available voices.
 
 engine.save_to_file(text, filename)
 engine.runAndWait()
